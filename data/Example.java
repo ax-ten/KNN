2c6a2c5762c40a0b1d9dd37e533f66903704cbf3
NaimarDL <nicoladario.losito@gmail.com>
2022-06-06 12:38:06 +0200

    Turn arrays into ArrayLists

--- data/Example.java
+++ data/Example.java
@@ -1,33 +1,34 @@
 package data;
 
+import java.util.*;
 import utility.ExampleSizeException;
 
 public class Example {
-    Object[] example;
+    private List<Object> example;
 
     public Example(int size) {
-        this.example = new Object[size];
+        this.example = new ArrayList<>(size);
     }
 
     public int getSize(){
-        return example.length;
+        return example.size();
     }
 
     public void set(Object o, int index) throws IndexOutOfBoundsException {
-        this.example[index] = o;
+        this.example.set(index, o);
     }
 
     public Object get(int index) throws IndexOutOfBoundsException {
-        return this.example[index];
+        return this.example.get(index);
     }
 
     public void swap(Example e) throws ExampleSizeException {
-        if (e.example.length != this.example.length) {
+        if (e.example.size() != this.example.size()) {
             throw new ExampleSizeException();
         } else {
-            Example temp = new Example(this.example.length);
+            Example temp = new Example(this.example.size());
 
-            for(int i = 0; i < this.example.length; ++i) {
+            for(int i = 0; i < this.example.size(); ++i) {
                 temp.set(this.get(i), i);
                 this.set(e.get(i), i);
                 e.set(temp.get(i), i);
@@ -39,17 +40,17 @@ public class Example {
     public String toString(){
         StringBuilder output = new StringBuilder();
         for (int i=0;i<this.getSize();i++){
-            output.append(String.format("%s,", this.example[i]));
+            output.append(String.format("%s,", this.example.get(i)));
         }
         return output.toString();
     }
 
     public double distance(Example e) throws ExampleSizeException {
         double d = 0.0D;
-        if (e.example.length != this.example.length) {
+        if (e.example.size() != this.example.size()) {
             throw new ExampleSizeException();
         } else {
-            for(int i = 0; i < e.example.length; ++i) {
+            for(int i = 0; i < e.example.size(); ++i) {
                 if (!this.get(i).equals(e.get(i))) {
                     ++d;
                 }
