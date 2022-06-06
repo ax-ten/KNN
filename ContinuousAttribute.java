e5c95695d94eebd36be97ab7b808bb35a5072541
Luigi Damato <gigdamato@gmail.com>
2022-06-06 13:12:12 +0200

    Removed doubled classes

--- ContinuousAttribute.java
+++
@@ -1,7 +0,0 @@
-import data.Attribute;
-
-public class ContinuousAttribute extends Attribute {
-    public ContinuousAttribute(String name, short index) {
-        super(name, index);
-    }
-}
--- DiscreteAttribute.java
+++
@@ -1,8 +0,0 @@
-import data.Attribute;
-
-
-public class DiscreteAttribute extends Attribute {
-    public DiscreteAttribute(String name, short index) {
-        super(name, index);
-    }
-}
