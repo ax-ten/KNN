c20918fc828fd52751e511ab1bddc55c5baf2341
NaimarDL <nicoladario.losito@gmail.com>
2022-06-07 10:43:37 +0200

    Updated ContinuousAttribute

--- data/ContinuousAttribute.java
+++ data/ContinuousAttribute.java
@@ -1,7 +1,24 @@
 package data;
 
 public class ContinuousAttribute extends Attribute {
+
+    private double min;
+    private double max;
+
     public ContinuousAttribute(String name, short index) {
         super(name, index);
+    }
+    //Aggiorna min in base al valore v passato come parametro
+    void setMin (Double v){
+        if (v < min) min = v;
+    }
+    //Aggiorna max in base al valore v passato come parametro
+    void setMax (Double v){
+        if (v > max) max = v;
+    }
+    
+    double scale (Double value){
+        double scale = (value-min)/(max-min);
+        return scale;
     }
 }
