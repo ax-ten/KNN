96acb47c3f01930d5b771b72367bfb91e0e3b174
Luigi Damato <gigdamato@gmail.com>
2022-09-17 03:32:47 +0200

    Add bot functionalities

--- Server/src/main/java/telegrambot/SimpleBot.java
+++ Server/src/main/java/telegrambot/SimpleBot.java
@@ -1,12 +1,22 @@
 package telegrambot;
 
+import data.Data;
+import mining.KNN;
 import pro.zackpollard.telegrambot.api.TelegramBot;
 import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
 import pro.zackpollard.telegrambot.api.event.Listener;
+import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
 import pro.zackpollard.telegrambot.api.event.chat.message.TextMessageReceivedEvent;
+import utility.DataUtility;
+import utility.KNNUtility;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
+
+import static utility.DataUtility.addMissingExtention;
 
 public class SimpleBot {
     public SimpleBot(){
@@ -45,8 +55,46 @@ public class SimpleBot {
 
     //Listener class
     private class MyListener implements Listener {
-        public void onTextMessageReceived(TextMessageReceivedEvent event) {
-            event.getChat().sendMessage(SendableTextMessage.builder().message("You sent me a text based message!").replyTo(event.getMessage()).build());
+
+        final String LOCALPATH = "src/main/Testfile/";
+        final String TXTEXT = ".dat";
+        final String BINEXT = ".dmp";
+
+        @Override
+        public void onCommandMessageReceived(CommandMessageReceivedEvent event) {
+            String filename ="";
+            try {
+                filename = event.getArgs()[0];
+            } catch (NullPointerException e){
+                e.printStackTrace();
+            }
+            Data trainingSet;
+            KNN knn = null;
+            try {
+                switch (event.getCommand().toLowerCase(Locale.ROOT)) {
+                    case "loadknnfromfile":
+                        trainingSet = DataUtility.getTrainingSetFromDat(addMissingExtention(filename, TXTEXT));
+                        reply(event,trainingSet.toString());
+                        new KNN(trainingSet).salva(LOCALPATH + filename + BINEXT); //Save KNN to binary
+                        break;
+                    case "loadknnfrombinary":
+                        knn = KNNUtility.loadKNNFromBin(addMissingExtention(filename, BINEXT));
+                        reply(event,knn.toString());
+                        break;
+                    case "loadknnfromdb":
+                        trainingSet = DataUtility.getTrainingSetFromDB(filename);
+                        reply(event,trainingSet.toString());
+                        new KNN(trainingSet).salva(LOCALPATH+filename+"DB"+BINEXT); //Save KNN to binary
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void reply(TextMessageReceivedEvent e, String s){
+            e.getChat().sendMessage(SendableTextMessage.builder().
+                    message(s).replyTo(e.getMessage()).build());
         }
     }
 }
