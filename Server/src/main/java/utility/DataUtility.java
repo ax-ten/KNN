0977296d3a4e08a6d547a338d58da642edb7c216
Luigi Damato <gigdamato@gmail.com>
2022-11-14 13:10:20 +0100

    Removed try/catch's from DataUtility

--- Server/src/main/java/utility/DataUtility.java
+++ Server/src/main/java/utility/DataUtility.java
@@ -18,38 +18,19 @@ public class DataUtility {
     static final String BINEXT = ".dmp";
 
 
-    public static KNN loadKNNFromBin(String filename)  {
-        KNN knn=new KNN();
-        try {
-            System.out.println(LOCALPATH+addMissingExtention(filename,BINEXT));
-            knn.carica(LOCALPATH + addMissingExtention(filename,BINEXT));
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return knn;
+    public static KNN loadKNNFromBin(String filename) throws IOException, ClassNotFoundException {
+        return new KNN().carica(LOCALPATH + addMissingExtention(filename,BINEXT));
     }
 
-    public static Data getTrainingSetFromDat(String filename) {
-        Data ts = null;
-        try {
-            ts =  new Data(LOCALPATH+addMissingExtention(filename,TXTEXT));
-        } catch (TrainingDataException e) {
-            e.printStackTrace();
-        }
-        return ts;
+    public static Data getTrainingSetFromDat(String filename) throws TrainingDataException {
+        return new Data(LOCALPATH+addMissingExtention(filename,TXTEXT));
     }
 
-    public static Data getTrainingSetFromDB(String tablename){
-        Data ts =null;
-        try {
-            DbAccess db = new DbAccess();
-            ts = new Data(db,tablename);
-            db.closeConnection();
-        }
-        catch(SQLException | TrainingDataException | DatabaseConnectionException |
-                NoValueException | InsufficientColumnNumberException exc){
-            System.out.println(exc.getMessage());
-        }
+    public static Data getTrainingSetFromDB(String tablename)
+            throws DatabaseConnectionException, NoValueException, SQLException, TrainingDataException, InsufficientColumnNumberException {
+         DbAccess db = new DbAccess();
+         Data ts = new Data(db,tablename);
+         db.closeConnection();
         return ts;
     }
 
