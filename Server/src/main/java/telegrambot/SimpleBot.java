165f2bde574f2a96e675aa49ab5a15f8e73c23e3
Luigi Damato <gigdamato@gmail.com>
2022-11-14 13:16:27 +0100

    Major update to Bot
    - Fix readability
    - minimize switch cases
    - add try/catch interaction to chat
    - add reply and send methods

--- Server/src/main/java/telegrambot/SimpleBot.java
+++ Server/src/main/java/telegrambot/SimpleBot.java
@@ -1,19 +1,28 @@
 package telegrambot;
 
 import data.Data;
+import data.TrainingDataException;
+import database.DatabaseConnectionException;
+import database.InsufficientColumnNumberException;
+import database.NoValueException;
 import example.ExampleSizeException;
 import mining.KNN;
 import pro.zackpollard.telegrambot.api.TelegramBot;
 import pro.zackpollard.telegrambot.api.chat.Chat;
+import pro.zackpollard.telegrambot.api.chat.message.send.ParseMode;
 import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
+import pro.zackpollard.telegrambot.api.event.Event;
 import pro.zackpollard.telegrambot.api.event.Listener;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
+import pro.zackpollard.telegrambot.api.event.chat.message.DocumentMessageReceivedEvent;
+import pro.zackpollard.telegrambot.api.event.chat.message.MessageReceivedEvent;
 import pro.zackpollard.telegrambot.api.event.chat.message.TextMessageReceivedEvent;
 import utility.DataUtility;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
 
 import static java.util.Objects.isNull;
@@ -24,17 +33,15 @@ public class SimpleBot {
         Scanner s = null;
         try {
             s = new Scanner(new File("src/main/java/server/AUTHKEY.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        } catch (FileNotFoundException e) {e.printStackTrace();}
 
         final String KEY = s.nextLine();
 
         TelegramBot tb = TelegramBot.login(KEY);
 
-        //The API key was invalid, an error will have also been printed into the console.
-        try{ convalidateAPIKey(tb);
-        } catch (InvalidBotException e) {e.printStackTrace();}
+        try{
+            convalidateAPIKey(tb);
+        } catch (InvalidBotException e)   {e.printStackTrace(); return;}
 
         //Register listeners
         tb.getEventsManager().register(new MyListener());
@@ -63,77 +70,161 @@ public class SimpleBot {
 
         @Override
         public void onTextMessageReceived(TextMessageReceivedEvent event) {
+            // i comandi sono considerati textmessages, tutti i comandi iniziano per '/', per cui
+            // escludiamo tutti i messaggi che iniziano così
             if (event.getMessage().toString().charAt(0)=='/'){return;}
             String chatID = event.getChat().getId();
             KNN knn;
             Double prediction;
+
+            // se l'utente ha registrato dei knn nel suo userdata (associato al suo id), ottieni il knn da lì
             if (userData.containsKey(chatID)){
                 knn = userData.get(chatID);
                 try {
+                    // predici, rispondi con la predizione e invia un messaggio che richiede nuovamente lo stesso format
+                    // per un'altra predizione sullo stesso knn
                     prediction = knn.predict(event.getContent().getContent());
-                    reply(event,"Prediction: "+prediction+ "\n"+exampleFormatBuilder(knn));
-                } catch (ExampleSizeException e) {
+                    reply(event,"Prediction: "+prediction);
+                    sendMessage(event.getChat(),exampleFormatBuilder(knn));
+                } catch (ExampleSizeException | NumberFormatException e) {
+                    // se il formato di predizione non è valido avvisa l'utente e richiede nuovamente lo stesso format
                     e.printStackTrace();
                     reply(event,"Formato non valido.\n" +exampleFormatBuilder(knn));
                 }
             } else
+                // altrimenti richiedi di caricare un file
                 event.getChat().sendMessage("Carica un trainingset per procedere.");
         }
 
         @Override
+        public void onDocumentMessageReceived(DocumentMessageReceivedEvent event) {
+            Data trainingSet;
+            String filename = event.getContent().getContent().getFileName();
+            event.getContent().getContent().
+                    downloadFile(event.getMessage().getBotInstance(), new File(LOCALPATH + filename));
+            try {
+                // carica trainingset dal file, ci crea un KNN e mostralo tramite messaggio, mostrando un prompt per
+                // richiedere anche un example di ingresso. salva poi il knn caricato
+                trainingSet = DataUtility.getTrainingSetFromDat(filename);
+                KNN knn = new KNN(trainingSet);
+                reply(event,knn.getData().toTgMessage());
+                sendMessage(event.getChat(),exampleFormatBuilder(knn));
+                userData.put(event.getChat().getId(),knn);
+            } catch (TrainingDataException e) {
+                e.printStackTrace();
+                event.getChat().sendMessage("File non valido, manda un file con estensione .dat per continuare");
+            }
+        }
+
+
+
+        @Override
         public void onCommandMessageReceived(CommandMessageReceivedEvent event) {
-            String filename ="", chatID = event.getChat().getId();
+            String
+                filename = "",
+                chatID = event.getChat().getId(),
+                command = event.getCommand().toLowerCase(Locale.ROOT);
+            boolean
+                flag_save = false,
+                flag_load = false;
+            KNN knn = null;
             try {
                 filename = event.getArgs()[0];
-            } catch (NullPointerException e){
-                e.printStackTrace();
+            } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
             }
-            Data trainingSet;
-            KNN knn = null;
-            boolean flag =true;
             try {
-                switch (event.getCommand().toLowerCase(Locale.ROOT)) {
-                    case "loadknnfromfile":
-                        trainingSet = DataUtility.getTrainingSetFromDat(filename);
-                        knn = new KNN(trainingSet); //Save KNN to binary
-                        knn.salva(LOCALPATH + filename + BINEXT);
-                        reply(event,knn.getData().toString());
-                        userData.put(chatID,knn);
+                switch (command) {
+                    case "loadfile":
+                        knn = new KNN(DataUtility.getTrainingSetFromDat(filename));
+                        flag_save = true;
                         break;
-                    case "loadknnfromdb":
-                        trainingSet = DataUtility.getTrainingSetFromDB(filename);
-                        knn = new KNN(trainingSet); //Save KNN to binary
-                        knn.salva(LOCALPATH + filename + BINEXT);
-                        reply(event,knn.getData().toString());
-                        userData.put(chatID,knn);
+                    case "loaddb":
+                        knn = new KNN(DataUtility.getTrainingSetFromDB(filename));
+                        flag_save = true;
                         break;
-                    case "loadknnfrombinary":
+                    case "loadbinary":
                         knn = DataUtility.loadKNNFromBin(filename);
-                        reply(event,knn.getData().toString());
-                        userData.put(chatID,knn);
+                        flag_load = true;
+                        break;
+                    case "listfiles":
+                        File folder = new File("src/main/Testfile/");
+                        StringBuilder msg = new StringBuilder("Lista dei file salvati sul server:\n```");
+                        for (File f: Objects.requireNonNull(folder.listFiles()))
+                            msg.append(f.getName()).append("\n");
+                        reply(event,msg.append("```").toString());
+                        break;
+                    case "start":
+                        reply(event, "Benvenutx in KNNbot! \nPer cominciare ad utilizzare questo bot utilizza uno " +
+                                "degli appositi comandi listati nel menù. Per i comandi di caricamento del KNN il bot " +
+                                "si aspetta anche il nome del file da aprire subito dopo il comando. Altrimenti puoi"+
+                                "caricare direttamente un file di testo (in formato .dat) e verrà automaticamente elaborato");
                         break;
                     default:
                         reply(event,"Comando non riconosciuto");
-                        flag = false;
                 }
-            } catch (IOException e) {
+
+            } catch (NullPointerException | ArrayIndexOutOfBoundsException | IOException | ClassNotFoundException e){
                 e.printStackTrace();
+                //flag_save = false;
+            } catch (TrainingDataException e) {
+                e.printStackTrace();
+                if (Objects.equals(filename, ""))
+                    reply(event, "Specifica il nome del file da caricare dopo il comando, ad esempio ` /loadfile provac`");
+                else if (command.equals("loaddb")) {
+                    reply(event, "Non ho trovato il database che cerchi");}
+                else
+                    reply(event, "Impossibile trovare il file specificato");
+            } catch (NoValueException | SQLException | InsufficientColumnNumberException e) {
+                e.printStackTrace();
+                reply(event, "Specifica il nome del file da caricare dopo il comando, ad esempio ` /loadfile provac`");
+            } catch (DatabaseConnectionException e) {
+                e.printStackTrace();
+                if (Objects.equals(filename, ""))
+                    reply(event, "Specifica il nome del database da caricare dopo il comando, ad esempio ` /loaddb provac`");
+                else
+                    reply(event, "Impossibile connettersi al DB");
             }
-            // se c'è un knn salvato, (se comando riconosciuto) manda il mex [C] [D] sul knn salvato
-            if (flag){
-                event.getChat().sendMessage(exampleFormatBuilder(knn));
+
+            if (flag_load || flag_save) {
+                sendMessage(event.getChat(),exampleFormatBuilder(knn) );
+                reply(event, knn.getData().toTgMessage());
+                userData.put(chatID, knn);
+            }
+            if (flag_save && !filename.equals("") ) {
+                try {
+                    knn.salva(LOCALPATH + filename + BINEXT);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    reply(event, "C'è stato un imprevisto col file selezionato");
+                }
             }
         }
 
-        private void reply(TextMessageReceivedEvent e, String s){
-            e.getChat().sendMessage(SendableTextMessage.builder().
-                    message(s).replyTo(e.getMessage()).build());
+        private void reply(MessageReceivedEvent e, List<String> s){
+            for (String message : s)
+                reply(e, message);
+        }
+
+        private void reply(MessageReceivedEvent e, String s){
+            e.getChat().sendMessage(SendableTextMessage.builder()
+                    .parseMode(ParseMode.MARKDOWN)
+                    .message(s)
+                    .replyTo(e.getMessage())
+                    .build());
+        }
+
+        private void sendMessage(Chat c, String s){
+            c.sendMessage(SendableTextMessage.builder()
+                    .parseMode(ParseMode.MARKDOWN)
+                    .message(s)
+                    .build());
         }
 
         private String exampleFormatBuilder(KNN knn){
-            return "Per procedere scrivere l'example in formato: \n"+
+            return "Per procedere scrivere l'example in formato: \n`"+
                     knn.getData().explanatorySetStringBuilder() +
-                    "\n[C]: Attributo continuo\n[D]: Attributo discreto\n[K]: Example target" +
+                    "`\n`[C]`: Attributo continuo\n`[D]`: Attributo discreto\n`[K]`: Example target" +
                     "\nOppure caricare un nuovo training set utilizzando gli appositi comandi.";
         }
     }
